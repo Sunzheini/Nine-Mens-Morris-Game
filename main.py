f2a19b03042044c5393407382f988e84211c0ef5
Sunzheini <daniel_zorov@abv.bg>
2022-09-19 13:39:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Tasks:
 2. valid / invalid fields marking
 
 while 1:
-3.     whist player is it
+3.     which player is it
 4.     number of pieces left to put ont the board before the turn
 5.     select place
 6.     check if selected place is valid
@@ -22,22 +22,88 @@ while 1:
 """
 
 # -----------------------------------------------------------------------
+
+
 # 1 - matrix 7 * 7
-FIELD_SIZE = 7
-field = []
+class Game:
+    FILE_PATH = ''
 
-# 2 - valid / invalid fields marking - how to print corrently???
-for _ in range(FIELD_SIZE):
-    field.append([0 for x in range(FIELD_SIZE)])
+    def __init__(self):
+        pass
 
-print(*field, sep='\n')
 
-while 1:
-    pass
-# 3 - whist player is it
+
+
+
+class PlayingField:
+    FIELD_SIZE = 7
+    F_SYMBOL = 'O'
+    H_SEP = '-'
+    V_SEP = '|'
+
+    def __init__(self):
+        self.matrix = []
+
+# 2 - valid / invalid fields marking
+    @property
+    def create_field(self):
+        self.matrix.append([self.F_SYMBOL, self.H_SEP, self.H_SEP, self.F_SYMBOL, self.H_SEP, self.H_SEP, self.F_SYMBOL])
+        self.matrix.append([self.V_SEP, self.F_SYMBOL, self.H_SEP, self.F_SYMBOL, self.H_SEP, self.F_SYMBOL, self.V_SEP])
+        self.matrix.append([self.V_SEP, self.V_SEP, self.F_SYMBOL, self.F_SYMBOL, self.F_SYMBOL, self.V_SEP, self.V_SEP])
+        self.matrix.append([self.F_SYMBOL, self.F_SYMBOL, self.F_SYMBOL, ' ', self.F_SYMBOL, self.F_SYMBOL, self.F_SYMBOL])
+        self.matrix.append([self.V_SEP, self.V_SEP, self.F_SYMBOL, self.F_SYMBOL, self.F_SYMBOL, self.V_SEP, self.V_SEP])
+        self.matrix.append([self.V_SEP, self.F_SYMBOL, self.H_SEP, self.F_SYMBOL, self.H_SEP, self.F_SYMBOL, self.V_SEP])
+        self.matrix.append([self.F_SYMBOL, self.H_SEP, self.H_SEP, self.F_SYMBOL, self.H_SEP, self.H_SEP, self.F_SYMBOL])
+
+        print(f"--Playing field {self.FIELD_SIZE}x{self.FIELD_SIZE} created--\n")
+
+    @property
+    def display_field(self):
+        for i in range(len(self.matrix)):
+            print(' '.join(self.matrix[i]))
+
+
+game.start_logging
+field = PlayingField()
+field.create_field
+field.display_field
+game.delete_log
+
+# each turn in a log (file handling)
+
+
+
+
+# while 1:
+#     pass
+# 3 - which player is it
+
+# da izpolzvam args i kwargs
+# error handling
+# props
+
+
 # 4 - number of pieces left to put ont the board before the turn
 # 5 - select place
 # 6 - check if selected place is valid
+
+# def is_outside(row, col, rows, cols):           # validaciq dali sa vytre
+#     return row < 0 or col < 0 or row >= rows or col >= cols
+
+# def accept_matrix_rows_and_columns():
+#     print("Select rows and columns of the matrix, separated by ', ': ")
+#     try:
+#         rows, columns = [int(i) for i in input().split(', ')]
+#         print(f"You have selected matrix size: {rows}, {columns}")
+#
+#         create_matrix_with_given_params(rows, columns)
+#         print_matrix(matrix)
+#
+#     except ValueError:
+#         print("---Input must two integers separated by ', '!---\n")
+#         accept_matrix_rows_and_columns()
+
+
 # 7 - check for the 3 x piece condition
 # 8 -       select to remove opponent's piece
 # 9 -       check if selection is valid
@@ -47,3 +113,6 @@ while 1:
 
 
 # 13. print final info
+
+
+# + testing
