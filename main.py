4ddcedf7a7a83096c6ca1e93af4b93cf8f755a7a
Sunzheini <daniel_zorov@abv.bg>
2022-09-19 12:16:37 +0300

    Update-19092022

---
+++ main.py
@@ -0,0 +1,49 @@
+# Nine Men's Morris
+
+"""
+Tasks:
+
+1. matrix 7 * 7
+2. valid / invalid fields marking
+
+while 1:
+3.     whist player is it
+4.     number of pieces left to put ont the board before the turn
+5.     select place
+6.     check if selected place is valid
+7.     check for the 3 x piece condition
+8.         select to remove opponent's piece
+9.         check if selection is valid
+10.        remove piece
+11.        print info about removal
+12.    print final turn info
+
+13. print final info
+"""
+
+# -----------------------------------------------------------------------
+# 1 - matrix 7 * 7
+FIELD_SIZE = 7
+field = []
+
+# 2 - valid / invalid fields marking - how to print corrently???
+for _ in range(FIELD_SIZE):
+    field.append([0 for x in range(FIELD_SIZE)])
+
+print(*field, sep='\n')
+
+while 1:
+    pass
+# 3 - whist player is it
+# 4 - number of pieces left to put ont the board before the turn
+# 5 - select place
+# 6 - check if selected place is valid
+# 7 - check for the 3 x piece condition
+# 8 -       select to remove opponent's piece
+# 9 -       check if selection is valid
+# 10 -      remove piece
+# 11 -      print info about removal
+# 12 - print final turn info
+
+
+# 13. print final info
